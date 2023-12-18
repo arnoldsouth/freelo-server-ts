6b78d9aa095ddd44e30278759fd0564d6d3630fc
arnoldsouth <arnoldsouth@gmail.com>
2023-12-18 12:20:37 -0800

    add league-exp api service, route, and controller

---
+++ MOCK_SUMMONER_DATA.ts
@@ -0,0 +1,17 @@
+export const summonerV4GetBySummonerName = {
+  id: 'cDyf7fzxsdz4ZzCiuu1bwktxmDy9GcstZfncyg6jwEIxaGM',
+  accountId: 'YhDTj8IpQJHyilBsJQLpvM95RzMYyEiIge_xRMpU8api7g',
+  puuid:
+    'tTCLUrmq3Oz8TQzOcpN0ia12ggJp8nuWmnbyqce9DFLC3rBKuV8yi-nHjG2HLQBqYfuVJPiNBZb--Q',
+  name: 'Doublelift',
+  profileIconId: 505,
+  revisionDate: 1702772502000,
+  summonerLevel: 423,
+};
+
+export const accountV1GetAccountByRiotId = {
+  puuid:
+    'tTCLUrmq3Oz8TQzOcpN0ia12ggJp8nuWmnbyqce9DFLC3rBKuV8yi-nHjG2HLQBqYfuVJPiNBZb--Q',
+  gameName: 'Doublelift',
+  tagLine: 'NA1',
+};
